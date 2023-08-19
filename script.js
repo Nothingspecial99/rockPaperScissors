2f87548eb2aca8256794f0a9722360a14d20884b
Nothingspecial99 <122666701+Nothingspecial99@users.noreply.github.com>
2023-08-19 13:56:50 +0530

    Add js to buttons

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function getComputerChoice(){
     const CHOICENUMBER = Math.floor(Math.random()*100);
     let computerChoice;
     if (CHOICENUMBER <= 33) {
-        computerChoice = "Rock"
+        computerChoice = "rock"
     }
     else if (CHOICENUMBER <= 66){
-        computerChoice = "Paper"
+        computerChoice = "paper"
     }
     else {
-        computerChoice = "Scissor"
+        computerChoice = "scissor"
     }
     return computerChoice
 }
@@ -35,49 +35,64 @@ function playRound(playerSelection, computerSelection){
     return outcome;
 }
 
-function capitalize(text){
-    firstLetter = text[0].toUpperCase();
-    restOfText = text.slice(1).toLowerCase();
 
-    return `${firstLetter}${restOfText}`
+function playerMove(event){
+    playerChoice = event.target.getAttribute('id');
+    computerChoice = getComputerChoice();
+
+    let playerScoreCard = document.querySelector('.player-score-card .score');
+    let computerScoreCard = document.querySelector('.computer-score-card .score');
+
+    let playerDisplay = document.querySelector('.player-display');
+    let computerDisplay = document.querySelector('.computer-display');
+
+    let winnerDisplay = document.querySelector("#winner");
+
+    //clearing previous choices border from display
+    clearCard(playerDisplay, computerDisplay);
+
+    showCard(playerChoice, computerChoice,playerDisplay, computerDisplay);
+    
+
+    winnerOfRound = playRound(playerChoice, computerChoice);
+
+    switch (winnerOfRound) {
+        case 'computer':
+            computerDisplay.style.border = '3px solid gold';
+            computerScoreCard.textContent = +computerScoreCard.textContent + 1;
+            break
+
+        case 'player':
+            playerDisplay.style.border = '3px solid gold';
+            playerScoreCard.textContent =  +playerScoreCard.textContent + 1;
+    }
+
+    if (+computerScoreCard.textContent ===5){
+        console.log('ran');
+        winnerDisplay.textContent = "Computer won, you loser!!!";
+        computerScoreCard.textContent = 0;
+        playerScoreCard.textContent =0;
+    }
+    else if (+playerScoreCard.textContent === 5){
+        winnerDisplay.textContent = "You won!!!";
+        computerScoreCard.textContent = 0;
+        playerScoreCard.textContent =0;
+    }
+
 
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let gameOutcome;
-
-    for (let i = 0; i < 5; i++){
-        let playerSelection = prompt('Enter your choice: ');
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-
-        if (result == 'player'){
-            playerScore += 1;
-            console.log(`${capitalize(result)} won! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`)
-        }
-        else if (result =='computer'){
-            computerScore += 1;
-            console.log(`${capitalize(result)} won! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`)
-
-        }
-        else{
-            console.log("Tie!")
-        }
-    }
-
-    console.log(`Player: ${playerScore}\nComputer: ${computerScore}`)
-    if (playerScore > computerScore){
-        gameOutcome = 'You won!'
-    }
-    else if (computerScore == playerScore){
-        gameOutcome = 'It was tie!'
-    }
-    else {
-        gameOutcome = "You lost :("
-    }
-    console.log(gameOutcome)
-
-
+function showCard(playerChoice, computerChoice, playerDisplay, computerDisplay) {
+    playerDisplay.textContent = playerChoice.toUpperCase();
+    computerDisplay.textContent = computerChoice.toUpperCase();
 }
+
+function clearCard(playerDisplay, computerDisplay){
+    playerDisplay.style.border = '1px solid white';
+    computerDisplay.style.border = '1px solid white';
+}
+
+
+const moveButtons = document.querySelectorAll('.choices button')
+
+moveButtons.forEach(button => button.addEventListener('click', playerMove))
